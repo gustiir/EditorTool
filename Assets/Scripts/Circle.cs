b883b35adc35cc621351502641a2ff9ed7d55d44
gustiir <gustiir@gmail.com>
2019-06-26 17:39:06 +0200

    First Commit

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Circle : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+   
+        public int vertexCount = 4;
+        public float lineWidth = 1f;
+        public float radius = 1f;
+
+    private void Awake()
+    {
+
+        lineRenderer = GetComponent<LineRenderer>();
+        SetupCircle();
+    }
+
+    private void SetupCircle()
+    {
+        lineRenderer.widthMultiplier = lineWidth;
+        float deltaTheta = (2f * Mathf.PI) / vertexCount;
+        float theta = 0f;
+
+        lineRenderer.positionCount = vertexCount;
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            Vector3 pos = new Vector3(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta), 0f);
+            lineRenderer.SetPosition(i, pos);
+            theta += deltaTheta;
+        }
+    }
+}
---
+++ Assets/Scripts/CircleWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CircleWindow : EditorWindow
+{
+    public Circle circle;
+
+    Component lineRenderer;
+
+    [MenuItem("Window/CircleWindow")]
+    public static void ShowWindow()
+    {
+        GetWindow<CircleWindow>("CircleWindow");
+    }
+
+    void OnGUI()
+    {
+        lineRenderer = (Component)EditorGUILayout.ObjectField(lineRenderer, typeof(LineRenderer), true);
+        circle = lineRenderer.GetComponent<Circle>();
+
+        GUILayout.Label("Sides");
+        circle.vertexCount = (int)EditorGUILayout.Slider(circle.vertexCount, 0, 360);
+
+        GUILayout.Label("Radius");
+        circle.radius = (float)EditorGUILayout.Slider(circle.radius, 0, 50);
+    }
+}
